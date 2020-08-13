3d45974f18b930b911158f873156b41c9a621223
Guilherme Nery <guilhermenc1@hotmail.com>
2020-08-13 17:17:41 -0300

    update models

--- app/Locations.php
+++ app/Locations.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Locations extends Model
 {
+    protected $fillable = ['name'];
+    protected $guarded = ['id', 'created_at', 'update_at'];
+    protected $table = 'printers';
+
     public function printers()
     {
         return $this->belongsToMany('App\Printers');
