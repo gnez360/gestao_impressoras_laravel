04a68c65eb555c65eb645d80fb09f0709b1f1f89
Guilherme Nery <guilhermenc1@hotmail.com>
2020-08-13 16:49:41 -0300

    update relationship with printers

--- app/Locations.php
+++ app/Locations.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Locations extends Model
 {
-    //
+    public function printers()
+    {
+        return $this->belongsToMany('App\Printers');
+    }
 }
